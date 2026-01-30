215e8356e5c36ea6fcde3e044460cdaaf1d8dfaa
Lőrintz Levente <lorintz.levente@mechwart.local>
2026-01-30 12:57:10 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿class Program{
+
+    static List <Dictionary<string,object>> dolgozokBeolvasas(){
+
+        List <Dictionary<string,object>> dolgozok = new List <Dictionary<string,object>>();
+
+        using(StreamReader sr = new StreamReader("berek2020.txt")){
+            sr.ReadLine();
+
+            string sor;
+            while((sor =sr.ReadLine()) != null){
+                string[] adatok = sor.Split(';');
+                var dolgozo= new Dictionary<string,object>();
+                dolgozo["nev"]=adatok[0];
+                dolgozo["neme"]=adatok[1];
+                dolgozo["reszleg"]=adatok[2];
+                dolgozo["belepes"]=adatok[3];
+                dolgozo["ber"]=adatok[4];
+
+                dolgozok.Add(dolgozo);
+            }
+        }
+        return dolgozok; 
+    }
+
+    static void kiir(List <Dictionary<string,object>> dolgozok){
+        foreach (var dolgozo in dolgozok)
+        {
+            Console.WriteLine(
+                $"Név: {dolgozo["nev"]}," +
+                $"Neme: {dolgozo["neme"]}," +
+                $"Részleg: {dolgozo["reszleg"]}," +
+                $"Belépés: {dolgozo["belepes"]}," +
+                $"Bér: {dolgozo["nev"]},"
+            );
+        }
+    }
+
+    static void Main(){
+
+        List <Dictionary<string,object>> Mechwart_dolgozoi= dolgozokBeolvasas();
+        kiir(Mechwart_dolgozoi); 
+
+    }
+}
